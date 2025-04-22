7fd513243fa28f97c30bb3a0fd5b487ca2747fce
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-04-22 20:58:58 +0300

    feat(service): implement DeploymentService for handling package and meta file upload.

---
+++ src/main/java/io/repsy/repository/service/DeploymentService.java
@@ -0,0 +1,42 @@
+package io.repsy.repository.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.repsy.repository.dto.MetaJsonDTO;
+import io.repsy.repository.model.PackageMetadata;
+import io.repsy.repository.repository.PackageMetadataRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class DeploymentService {
+
+    private final PackageMetadataRepository repository;
+    private final StorageService storageService;
+    private final ObjectMapper objectMapper;
+
+    public void deploy(String packageName, String version, MultipartFile packageFile,MultipartFile metaFile) throws IOException {
+
+        MetaJsonDTO meta = objectMapper.readValue(metaFile.getBytes(),MetaJsonDTO.class);
+
+        if (!packageName.equals(meta.getName()) || !version.equals(meta.getVersion())) {
+            throw new IllegalArgumentException("Path and meta.json version/name mismatch");
+        }
+
+        String basePath = packageName + "/" + version;
+        String packagePath = storageService.store(basePath+ "/package.rep", packageFile.getInputStream());
+        String metaPath = storageService.store(basePath + "/meta.json", metaFile.getInputStream());
+    }
+
+    PackageMetadata entity = new PackageMetadata();
+    entity.setName(meta.getName());
+    entity.setVersion(meta.getVersion());
+    entity.setAuthor(meta.getAuthor());
+    entity.setDependencies(meta.getDependencies());
+    entity.setFilePath(packagePath);
+    entity.setMetaPath(metaPath);
+
+    repository.save(entity)
+}
