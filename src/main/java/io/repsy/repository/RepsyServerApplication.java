ddfecb5f4b99ed54ffbe4f4d1fbc1f82e473b123
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-04-22 20:13:00 +0300

    all packages are created.

---
+++ src/main/java/io/repsy/repository/RepsyServerApplication.java
@@ -0,0 +1,13 @@
+package io.repsy.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepsyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepsyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/repsy/repository/RepsyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.repsy.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepsyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
