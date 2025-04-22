85e84e6aa498e5a48b763a4b4db16bcee7ed7d6a
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-04-22 21:25:58 +0300

    feat(controller): add DeploymentController for handling package uploads via multipart

---
+++ src/main/java/io/repsy/repository/controller/DeploymentController.java
@@ -0,0 +1,32 @@
+package io.repsy.repository.controller;
+
+import io.repsy.repository.service.DeploymentService;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/{packageName/version")
+public class DeploymentController {
+
+    private final DeploymentService deploymentService;
+
+    @PostMapping
+    public ResponseEntity<String> uploadPackage(
+            @PathVariable @NotBlank String packageName,
+            @PathVariable @NotBlank String version,
+            @RequestPart("packageFile")MultipartFile packageFile,
+            @RequestPart("metafile") MultipartFile metaFile
+            ) {
+        try{
+            deploymentService.deploy(packageName,version,packageFile, metaFile);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Deployment successful");
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Deployment failed: " + e.getMessage());
+        }
+    }
+}
